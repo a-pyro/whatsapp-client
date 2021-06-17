0e654a153c708501c6c21c1799decee7142ab1e8
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-06-17 15:18:07 +0200

    adds landing component

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,16 +1,38 @@
 import { MessageBox } from 'react-chat-elements';
 import { SideBar } from 'react-chat-elements';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import Login from './Login';
+import { History } from 'history';
 
-const Landing = () => {
+interface Props {
+  history: History;
+}
+
+const Landing = ({ history }: Props) => {
   return (
-    <div>
-      <SideBar
-        top={<div>'TOP' area</div>}
-        center={<div> hi</div>}
-        bottom={<div>'BOTTOM' area</div>}
-      />
-    </div>
+    <Container fluid style={{ background: 'teal' }}>
+      <Row className='justify-content-center align-items-center min-vh-100'>
+        <Col sm={6}>
+          <h1 className='mb-4 text-white'>Welcome to Whatsapp(Fake)</h1>
+          <div className='d-flex flex-column'>
+            <Button
+              variant='warning'
+              onClick={() => history.push(`/login`)}
+              className='rounded-pill mb-3'
+            >
+              Login
+            </Button>
+            <Button
+              variant='info'
+              onClick={() => history.push(`/register`)}
+              className='rounded-pill text-white'
+            >
+              Register
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
