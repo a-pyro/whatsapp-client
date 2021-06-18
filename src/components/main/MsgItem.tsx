07e676839fdce86cc4d742e7e28a979a539e4af8
Mihai Ivanov <mikee2988@yahoo.com>
2021-06-18 15:40:42 +0300

    added messageItem

--- src/components/main/MsgItem.tsx
+++ src/components/main/MsgItem.tsx
@@ -1,5 +1,53 @@
-const MsgItem = () => {
-  return <div></div>;
+import styled from "styled-components";
+import moment from "moment";
+
+const MsgItem = ({ message }: any) => {
+  const userLoggedIn = localStorage.getItem("user_logged_in");
+
+  const TypeOfMessage = message.senderId === userLoggedIn ? Sender : Receiver;
+
+  return (
+    <Container>
+      <TypeOfMessage>
+        {message.text}
+        <Timestamp>
+          {message.createdAt ? moment(message.createdAt).format("LT") : "..."}
+        </Timestamp>
+      </TypeOfMessage>
+    </Container>
+  );
 };
 
 export default MsgItem;
+
+const Container = styled.div``;
+
+const MessageElement = styled.p`
+  width: fit-content;
+  padding: 15px;
+  border-radius: 8px;
+  margin: 10px;
+  min-width: 60px;
+  padding-bottom: 26px;
+  position: relative;
+  text-align: right;
+`;
+
+const Sender = styled(MessageElement)`
+  margin-left: auto;
+  background-color: #dcf8c6;
+`;
+const Receiver = styled(MessageElement)`
+  text-align: left;
+  background-color: whitesmoke;
+`;
+
+const Timestamp = styled.span`
+  color: gray;
+  padding: 10px;
+  font-size: 9px;
+  position: absolute;
+  bottom: 0;
+  text-align: right;
+  right: 0;
+`;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,9 @@
-import { Form, Container, Row, Col, Button } from 'react-bootstrap';
+import { Form, Container, Row, Col, Button } from "react-bootstrap";
 // import { History } from 'history';
-import { useState } from 'react';
-import { History } from 'history';
-import React from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { useState } from "react";
+import { History } from "history";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 interface FormFields {
   email: string;
@@ -15,7 +15,7 @@ interface Props {
 }
 
 const Login = ({ history }: Props) => {
-  const [fields, setFields] = useState<FormFields>({ email: '', password: '' });
+  const [fields, setFields] = useState<FormFields>({ email: "", password: "" });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const key = e.target.type;
@@ -28,25 +28,30 @@ const Login = ({ history }: Props) => {
 
     try {
       const resp = await fetch(process.env.REACT_APP_API_URL + `/login`, {
-        method: 'POST',
+        method: "POST",
         body: JSON.stringify(fields),
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        credentials: 'include',
+        credentials: "include",
       });
-      console.log(resp);
 
       if (resp.ok) {
-        // localStorage.setItem('user_logged_in', user._id);
-        toast('Yay! Redirecting to you chats!');
+        const getMe = await fetch(process.env.REACT_APP_API_URL + "/users/me", {
+          credentials: "include",
+        });
+        const user = await getMe.json();
+        console.log(user);
+
+        localStorage.setItem("user_logged_in", user._id);
+        toast("Yay! Redirecting to you chats!");
         setTimeout(() => {
-          history.push('/home');
+          history.push("/home");
         }, 2000);
       } else {
-        toast.error('something bad happend, try again');
+        toast.error("something bad happend, try again");
         setTimeout(() => {
-          history.push('/login');
+          history.push("/login");
         }, 2000);
       }
     } catch (error) {
@@ -55,38 +60,38 @@ const Login = ({ history }: Props) => {
   };
 
   return (
-    <Container fluid style={{ background: 'teal' }}>
-      <Row className='justify-content-center align-items-center min-vh-100'>
+    <Container fluid style={{ background: "teal" }}>
+      <Row className="justify-content-center align-items-center min-vh-100">
         <Col sm={6}>
-          <h4 className='mb-4 text-white'>Please Login</h4>
-          <div className='d-flex flex-column'>
+          <h4 className="mb-4 text-white">Please Login</h4>
+          <div className="d-flex flex-column">
             <Form onSubmit={handleSubmit}>
-              <Form.Group controlId='formBasicEmail'>
+              <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control
                   onChange={handleChange}
-                  className='rounded-pill'
-                  type='email'
-                  placeholder='Enter email'
+                  className="rounded-pill"
+                  type="email"
+                  placeholder="Enter email"
                 />
               </Form.Group>
 
-              <Form.Group controlId='formBasicPassword' className='mt-2'>
+              <Form.Group controlId="formBasicPassword" className="mt-2">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
                   onChange={handleChange}
-                  className='rounded-pill'
-                  type='password'
-                  placeholder='Password'
+                  className="rounded-pill"
+                  type="password"
+                  placeholder="Password"
                 />
               </Form.Group>
 
               <Button
-                variant='warning'
-                className='rounded-pill mt-3'
-                type='submit'
+                variant="warning"
+                className="rounded-pill mt-3"
+                type="submit"
               >
-                {' '}
+                {" "}
                 Login
               </Button>
             </Form>
